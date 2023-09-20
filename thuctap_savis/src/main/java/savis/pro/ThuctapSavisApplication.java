f362b68e8a7ab42e2961a843af3c6a9bd3da7a76
nguyendinhduyso <sonnddph26841@fpt.edu.vn>
2023-09-20 09:08:13 +0700

    first commit

---
+++ thuctap_savis/src/main/java/savis/pro/ThuctapSavisApplication.java
@@ -0,0 +1,13 @@
+package savis.pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThuctapSavisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThuctapSavisApplication.class, args);
+	}
+
+}
